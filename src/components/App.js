d34d1064037be056c3effe410811b22c51ec0b8e
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-21 14:28:01 +0530

    test 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,74 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles/App.css';
-const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+// import React, { useReducer } from 'react'
+// import { counterReducer } from '../reducers/counterReducer';
+import "../styles/App.css";
+// const App = () => {
+// const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+//   return (
+//     <div id="main">
+
+//     </div>
+//   )
+// }
+
+// export default App;
+import React, { createContext, useContext, useReducer } from "react";
+
+const initialState = {
+  count1: 0,
+  count2: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        [action.name]: state[action.name] + 1
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        [action.name]: state[action.name] - 1
+      };
+    default:
+      return state;
+  }
+};
+
+const useValue = () => useReducer(reducer, initialState);
+
+const Context = createContext(null);
+
+const useGlobalState = () => {
+  const value = useContext(Context);
+  if (value === null) throw new Error("Please add GlobalStateProvider");
+  return value;
+};
+
+const GlobalStateProvider = ({ children }) => (
+  <Context.Provider value={useValue()}>{children}</Context.Provider>
+);
+
+const Counter = ({ name }) => {
+  const [state, dispatch] = useGlobalState();
   return (
     <div id="main">
-
+      {state[name]}
+      <button onClick={() => dispatch({ type: "INCREMENT", name })}>+1</button>
+      <button onClick={() => dispatch({ type: "DECREMENT", name })}>-1</button>
     </div>
-  )
-}
+  );
+};
 
+const App = () => (
+  <GlobalStateProvider>
+    <h1>Count1</h1>
+    <Counter name="count1" />
+    {/* <Counter name="count1" /> */}
+    {/* <h1>Count2</h1>
+    <Counter name="count2" />
+    <Counter name="count2" /> */}
+  </GlobalStateProvider>
+);
 
 export default App;
