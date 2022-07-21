c423e3cc9f9bff2110ae47a606ed1df94fffd108
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-21 14:31:33 +0530

    test 2

--- src/components/App.js
+++ src/components/App.js
@@ -10,65 +10,90 @@ import "../styles/App.css";
 //   )
 // }
 
-// export default App;
-import React, { createContext, useContext, useReducer } from "react";
+// // export default App;
+// import React, { createContext, useContext, useReducer } from "react";
 
-const initialState = {
-  count1: 0,
-  count2: 0
-};
+// const initialState = {
+//   count1: 0,
+//   count2: 0
+// };
 
-const reducer = (state, action) => {
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return {
+//         ...state,
+//         [action.name]: state[action.name] + 1
+//       };
+//     case "DECREMENT":
+//       return {
+//         ...state,
+//         [action.name]: state[action.name] - 1
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// const useValue = () => useReducer(reducer, initialState);
+
+// const Context = createContext(null);
+
+// const useGlobalState = () => {
+//   const value = useContext(Context);
+//   if (value === null) throw new Error("Please add GlobalStateProvider");
+//   return value;
+// };
+
+// const GlobalStateProvider = ({ children }) => (
+//   <Context.Provider value={useValue()}>{children}</Context.Provider>
+// );
+
+// const Counter = ({ name }) => {
+//   const [state, dispatch] = useGlobalState();
+//   return (
+//     <div id="main">
+//       {state[name]}
+//       <button onClick={() => dispatch({ type: "INCREMENT", name })}>+1</button>
+//       <button onClick={() => dispatch({ type: "DECREMENT", name })}>-1</button>
+//     </div>
+//   );
+// };
+
+// const App = () => (
+//   <GlobalStateProvider>
+//     <h1>Count1</h1>
+//     <Counter name="count1" />
+//     {/* <Counter name="count1" /> */}
+//     {/* <h1>Count2</h1>
+//     <Counter name="count2" />
+//     <Counter name="count2" /> */}
+//   </GlobalStateProvider>
+// );
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
   switch (action.type) {
-    case "INCREMENT":
-      return {
-        ...state,
-        [action.name]: state[action.name] + 1
-      };
-    case "DECREMENT":
-      return {
-        ...state,
-        [action.name]: state[action.name] - 1
-      };
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
     default:
-      return state;
+      throw new Error();
   }
-};
+}
 
-const useValue = () => useReducer(reducer, initialState);
-
-const Context = createContext(null);
-
-const useGlobalState = () => {
-  const value = useContext(Context);
-  if (value === null) throw new Error("Please add GlobalStateProvider");
-  return value;
-};
-
-const GlobalStateProvider = ({ children }) => (
-  <Context.Provider value={useValue()}>{children}</Context.Provider>
-);
-
-const Counter = ({ name }) => {
-  const [state, dispatch] = useGlobalState();
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div id="main">
-      {state[name]}
-      <button onClick={() => dispatch({ type: "INCREMENT", name })}>+1</button>
-      <button onClick={() => dispatch({ type: "DECREMENT", name })}>-1</button>
-    </div>
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </>
   );
-};
-
-const App = () => (
-  <GlobalStateProvider>
-    <h1>Count1</h1>
-    <Counter name="count1" />
-    {/* <Counter name="count1" /> */}
-    {/* <h1>Count2</h1>
-    <Counter name="count2" />
-    <Counter name="count2" /> */}
-  </GlobalStateProvider>
-);
-
+}
 export default App;
